9a81775fd9acbe25c9b8784f7c0a82ac67fc59f4
Sharvari Thombre <thombresharvari@gmail.com>
2021-02-23 14:15:08 +0530

    modified test file

--- src/test/java/com/sharvari/SpringDockerApplicationTests.java
+++ src/test/java/com/sharvari/SpringDockerApplicationTests.java
@@ -3,24 +3,17 @@ package com.sharvari;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
-
 
 @SpringBootTest
-@TestPropertySource("/application-prod.properties")
 class SpringDockerApplicationTests {
 	private final static int EXPECTED_PORT = 8190;
 	@Value("${server.port}")
 	private int serverPort;
-	@Autowired
-	WelcomeController welcomeController;
 	@Test
 	void contextLoads() {
 		assertEquals(EXPECTED_PORT,serverPort);
 	}
 
 }
-
