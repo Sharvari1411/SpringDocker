e731c5e4601a367210d0afa4de59c7ba7d84abe7
Sharvari Thombre <thombresharvari@gmail.com>
2021-02-23 13:22:36 +0530

    tests added

--- src/test/java/com/sharvari/SpringDockerApplicationTests.java
+++ src/test/java/com/sharvari/SpringDockerApplicationTests.java
@@ -5,9 +5,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringDockerApplicationTests {
-
+	private final static int EXPECTED_PORT = 7777;
+	@Value("${server.port}")
+	private int serverPort;
 	@Test
 	void contextLoads() {
+		assertEquals(EXPECTED_PORT,serverPort);
 	}
 
 }
