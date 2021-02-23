c4fd35d07733d1fecdd1146443276f406281980f
Sharvari Thombre <thombresharvari@gmail.com>
2021-02-23 14:06:19 +0530

    .......

--- src/test/java/com/sharvari/SpringDockerApplicationTests.java
+++ src/test/java/com/sharvari/SpringDockerApplicationTests.java
@@ -4,7 +4,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
 
 @SpringBootTest
 @TestPropertySource("/application-prod.properties")
